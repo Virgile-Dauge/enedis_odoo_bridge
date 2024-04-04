d3e30b75a43d1667bd2a44212412c403e41be1c7
Virgile <virgile.dauge@pm.me>
2024-04-04 19:44:27 +0200

    Maj format logs

--- src/enedis_odoo_bridge/skeleton.py
+++ src/enedis_odoo_bridge/skeleton.py
@@ -91,7 +91,7 @@ def setup_logging(loglevel):
     Args:
       loglevel (int): minimum loglevel for emitting messages
     """
-    logformat = "[%(asctime)s] %(levelname)s:%(name)s:%(message)s"
+    logformat = "[%(asctime)s] %(levelname)s:%(name)s: %(message)s"
     logging.basicConfig(
         level=loglevel, stream=sys.stdout, format=logformat, datefmt="%Y-%m-%d %H:%M:%S"
     )
