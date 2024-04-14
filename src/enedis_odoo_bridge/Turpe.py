a30e567af6b881f87f33e92f83360eda90fa0565
Virgile <virgile.dauge@pm.me>
2024-04-14 22:16:08 +0200

    Ajout tests pour le turpe

--- src/enedis_odoo_bridge/Turpe.py
+++ src/enedis_odoo_bridge/Turpe.py
@@ -12,9 +12,21 @@ class Turpe:
     def __init__(self):
         load_dotenv()
         self.constants = {k: float(v) for k, v in os.environ.items() if k.startswith('TURPE_')}
+
+        required = ['TURPE_TAUX_HPH_CU4', 'TURPE_TAUX_HCH_CU4', 
+                    'TURPE_TAUX_HPB_CU4', 'TURPE_TAUX_HCB_CU4', 
+                    'TURPE_B_CU4', 'TURPE_CG', 'TURPE_CC']
+        print(self.constants)
+        if not all(c in self.constants for c in required):
+            raise OSError(f'Required constants {required} not found in .env file')
+        
         _logger.info(f'Turpe constants from .env : {pretty_repr(self.constants)}')
 
     def compute(self, releves: pd.DataFrame) -> pd.DataFrame:
+        required = ['HPH_conso', 'HCH_conso', 'HPB_conso', 'HCB_conso', 'puissance_souscrite']
+        if not all(c in releves.columns for c in required):
+            raise ValueError(f'Required data {required} not found in imput releves')
+        
         releves['turpe'] = sum([
             releves['HPH_conso'].astype(float)*self.constants['TURPE_TAUX_HPH_CU4']*0.01,
             releves['HCH_conso'].astype(float)*self.constants['TURPE_TAUX_HCH_CU4']*0.01,
