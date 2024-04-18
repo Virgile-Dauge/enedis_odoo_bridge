4652d99228d683d59d3e6ecbb2821402fbb45e2d
Virgile <virgile.dauge@pm.me>
2024-04-18 15:32:32 +0200

    Added loggingg

--- src/enedis_odoo_bridge/DataMerger.py
+++ src/enedis_odoo_bridge/DataMerger.py
@@ -1,10 +1,13 @@
 from typing import Dict, List
 from datetime import date
+from pandas import DataFrame
 
 from enedis_odoo_bridge.OdooAPI import OdooAPI
 from enedis_odoo_bridge.EnedisFluxEngine import EnedisFluxEngine
 from enedis_odoo_bridge.utils import gen_dates, check_required
 
+import logging
+_logger = logging.getLogger(__name__)
 
 class DataMerger:
     def __init__(self, config: Dict[str, str], date:date, enedis: EnedisFluxEngine, odoo: OdooAPI) -> None:
@@ -15,17 +18,22 @@ class DataMerger:
         self.starting_date, self.ending_date = gen_dates(date)
 
 
-    def fetch_enedis_data(self):
+    def fetch_enedis_data(self)-> DataFrame:
         # Récupérer les données depuis EnedisFluxEngine
         return self.enedis.estimate_consumption(self.starting_date, self.ending_date)
 
-    def fetch_odoo_data(self):
+    def fetch_odoo_data(self)-> DataFrame:
         # Récupérer les données depuis OdooAPI
         return self.odoo.get_drafts()
 
-    def merge_data(self, enedis_data, odoo_data):
+    def merge_data(self, enedis_data: DataFrame, odoo_data: DataFrame)-> DataFrame:
         # Fusionner les données ici
-        pass
+        _logger.info(f"Merging data:")
+        _logger.info(f"- {len(enedis_data)} enedis entries.")
+        _logger.debug(enedis_data)
+        _logger.info(f"- {len(odoo_data)} odoo entries.")
+        _logger.debug(odoo_data)
+        return DataFrame({})
 
     def update_odoo(self, merged_data):
         # Mettre à jour Odoo avec les données fusionnées
