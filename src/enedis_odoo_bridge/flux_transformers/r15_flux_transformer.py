33094064f9796db10785957d70053d24d18575e7
Virgile <virgile.dauge@pm.me>
2024-04-22 17:51:27 +0200

    Ajout de preprocesseurs

--- src/enedis_odoo_bridge/flux_transformers/r15_flux_transformer.py
+++ src/enedis_odoo_bridge/flux_transformers/r15_flux_transformer.py
@@ -38,8 +38,7 @@ class R15FluxTransformer(BaseFluxTransformer):
                 rows.append(row)
         df = DataFrame(rows)
         columns = [tuple(k.split('.')) for k in df.columns]
-        #if len(columns) != len(df.columns):
-        print(columns, df.columns)
+
         column_renaming = {
             # Add more columns to rename here
             # 'some_old_column_name': 'new_column_name',
@@ -49,16 +48,49 @@ class R15FluxTransformer(BaseFluxTransformer):
         multi_index_columns = pd.MultiIndex.from_tuples(columns)
         # Assign the MultiIndex to your DataFrame's columns
         df.columns = multi_index_columns
-        print(columns, df.columns)
         return df
+    
+    def compress_serial_number(self, data: DataFrame)-> DataFrame:
+        # Function to check if all 'Num_Serie' values in a row are the same and return the value if true
+        def check_num_serie_uniformity(row):
+            unique_values = row.dropna().unique().astype(str)
+            if len(unique_values) == 1:
+                return unique_values[0]
+            else:
+                raise ValueError("Different 'Num_Serie' values found within the same row.")
+
+        # Apply the function across the DataFrame to create a new 'Num_Serie' column
+        # Extract only the 'Num_Serie' columns for this operation
+        num_serie_columns = [col for col in data.columns if col[2] == 'Num_Serie']
+        data[('', 'meta', 'Num_Serie')] = data[num_serie_columns].apply(check_num_serie_uniformity, axis=1)
+        # Optionally, drop the original 'Num_Serie' columns if they are no longer needed
+        return data.drop(columns=num_serie_columns)
+
+    def exact_drop(self, data: DataFrame, keys_to_drop: list[str])-> DataFrame:
+        level = data.columns.nlevels-1
+        to_drop = [col for col in data.columns if col[level] in keys_to_drop]
+        return data.drop(columns=to_drop)
+    
+    def endswith_drop(self, data: DataFrame, keys_to_drop: list[str])-> DataFrame:
+        level = data.columns.nlevels-1
+        to_drop = [col for col in self.data.columns if any(col[level].endswith(k) for k in keys_to_drop)]
+        return data.drop(columns=to_drop)
     
     def preprocess(self) -> DataFrame:
         self.data = self.data.sort_values(by=[('', 'meta', 'pdl'), ('', 'meta', 'Date_Releve')],)
         self.data = self.data.reset_index(drop=True)
 
-        # TODO : DROP all Id_Classe_Temporelle, all Libelle_Classe_Temporelle Classe_Mesure
-        # Identify columns to drop where the lower level of the MultiIndex ends with "Classe_Temporelle"
-        to_drop = [col for col in self.data.columns if col[2].endswith("Classe_Temporelle")]
-        self.data = self.data.drop(columns=to_drop)
-        # TODO : Un seul num série Num_Serie
+        self.data = self.compress_serial_number(self.data)
+        self.data = self.exact_drop(self.data, ['Unite_Mesure',
+                                                'Classe_Mesure', 
+                                                'Id_Classe_Temporelle',
+                                                'Libelle_Classe_Temporelle', ])
+        self.data = self.endswith_drop(self.data, ['Classe_Temporelle'])
+        
+        # TODO To DATE 
+        # Convert columns where the last level of the index starts with "Date_" to datetime
+        for col in self.data.columns:
+            if col[2].startswith("Date_"):
+                self.data[col] = pd.to_datetime(self.data[col])
+
         return self.data
