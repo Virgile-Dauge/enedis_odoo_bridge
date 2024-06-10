433c0a28878314ee683ed40a9d3851bb3fc47f2e
Virgile <virgile.dauge@pm.me>
2024-06-10 11:04:26 +0200

    remove enedis_flux_reader

--- src/enedis_odoo_bridge/enedis_flux_reader/__init__.py
+++
@@ -1,3 +0,0 @@
-from .services import get_consumptions_by_date
-from .services import get_r15_by_date
-from .services import get_f15_by_date
--- src/enedis_odoo_bridge/enedis_flux_reader/flux_repository/__init__.py
+++
@@ -1,3 +0,0 @@
-from .base_flux_repository import BaseFluxRepository
-from .flux_repository_factory import FluxRepositoryFactory
-
