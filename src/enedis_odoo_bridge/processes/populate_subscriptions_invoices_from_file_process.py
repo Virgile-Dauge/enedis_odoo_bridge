c5e619c73750ac5475c5c9588a45599eda8c7b36
Virgile <virgile.dauge@pm.me>
2024-07-02 18:08:24 +0200

    ajout logs et fichiers pour debug

--- src/enedis_odoo_bridge/processes/populate_subscriptions_invoices_from_file_process.py
+++ src/enedis_odoo_bridge/processes/populate_subscriptions_invoices_from_file_process.py
@@ -21,22 +21,26 @@ class PopulateSubscriptionsInvoicesFromFileProcess(BaseProcess):
                 logger: logging.Logger=logging.getLogger('enedis_odoo_bridge')) -> None:
         super().__init__(config, enedis) 
         self.filter = filter
+        self.logger = logger
         self.odoo = odoo
         self.starting_date, self.ending_date = gen_Timestamps(date)
         self.data_path = data_path
         
     def run(self):
-        consumptions = pd.read_csv(self.data_path)[['pdl', 'F_HP', 'F_HC', 'F_BASE', 'TURPE']].fillna(0)
+        self.logger.info("Lecture du fichier de consommations")
+        consumptions = pd.read_csv(self.data_path, encoding='ISO-8859-1', skiprows=1)[['pdl', 'F_HP', 'F_HC', 'F_BASE', 'TURPE']].fillna(0)
+        self.logger.info("Conversion des colonnes F_HP, F_HC, F_BASE en int et TURPE en float")
         consumptions['F_HP'] = consumptions['F_HP'].astype(int)
         consumptions['F_HC'] = consumptions['F_HC'].astype(int)
         consumptions['F_BASE'] = consumptions['F_BASE'].astype(int)
         consumptions['TURPE'] = consumptions['TURPE'].apply(lambda x: float(str(x).replace(',', '.')))
         consumptions['pdl'] = consumptions['pdl'].apply(lambda x: f"{int(x)}")
+        self.logger.info("Suppression des lignes avec pdl manquant ou égal à 0")
         consumptions = consumptions.dropna(subset=['pdl'])
         consumptions = consumptions[consumptions['pdl'] != "0"]
         consumptions['not_enough_data'] = False
         consumptions = consumptions.rename(columns={'F_HP': 'HP', 'F_HC': 'HC', 'F_BASE': 'Base', 'TURPE': 'x_turpe'})
-        print(consumptions)
+        self.logger.info(f" #{len(consumptions)} Consommations traitées:\n{consumptions}")
 
 
         # Récup données Odoo
@@ -44,13 +48,14 @@ class PopulateSubscriptionsInvoicesFromFileProcess(BaseProcess):
 
         draft_orders['invoice_ids'] = draft_orders['invoice_ids'].apply(lambda x: max(x) if x else None)
         draft_orders = draft_orders.rename(columns={'sale.order_id': 'order_id', 'invoice_ids': 'move_id'})
-        print(draft_orders)
+        self.logger.info(f" #{len(draft_orders)} Draft orders:\n{draft_orders}")
 
         draft_invoices = self.odoo.read('account.move', ids=draft_orders['move_id'].to_list(), fields=['invoice_line_ids',])
         draft_orders['invoice_line_ids'] = draft_invoices['invoice_line_ids']
-        print(draft_invoices)
+        #self.logger.info("Draft invoices:\n%s", draft_invoices)
 
         odoo_data = self.odoo.add_cat_fields(draft_orders, [])
+        self.logger.info(f"# {len(draft_invoices)} Draft invoices:\n{draft_invoices}")
 
         # Récup données Enedis
         columns = ['pdl', 'Type_Compteur', 'Num_Serie', 'Date_Theorique_Prochaine_Releve', 
@@ -59,14 +64,14 @@ class PopulateSubscriptionsInvoicesFromFileProcess(BaseProcess):
                                 columns=['Type_Compteur', 'Num_Serie', 'Date_Theorique_Prochaine_Releve'],
                                 heuristic=LastFirstEstimator())[columns]
         
-        print(enedis_data)
-
+        self.logger.info(f"# {len(enedis_data)} Enedis data:\n{enedis_data}")
+        enedis_data.to_csv('enedis_data.csv')
         # Merge les 3 sources
         merged_data = pd.merge(odoo_data, consumptions, left_on='x_pdl', right_on='pdl', how='inner').drop(columns=['pdl'])
         # Merging all data
         merged_data = pd.merge(merged_data, enedis_data, left_on='x_pdl', right_on='pdl', how='inner').drop(columns=['pdl'])
-        print(merged_data)
-
+        self.logger.info(f"# {len(merged_data)} Merged data:\n{merged_data}")
+        merged_data.to_csv('merged_data.csv')
         self.odoo.update_draft_invoices(merged_data, self.starting_date, self.ending_date)
 
 
